421c71156181c18e025f4a950bfd124e0dc33548
Anatoly Ivashov <tonyshow@mail.ru>
2023-02-01 20:12:09 +0200

    Added React Router + Code splitting to chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from './components/Counter'
+import { Suspense } from 'react'
+import { Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './index.scss'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
 
 const App = () => {
   return (
     <div className='app'>
-      ABC
-      <Counter />
+      <Link to='/'>Главная</Link>
+      <Link to='/about'>About</Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path='/about' element={<AboutPageAsync />} />
+          <Route path='/' element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
