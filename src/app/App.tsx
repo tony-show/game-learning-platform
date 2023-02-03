0c8a4354f5bd806ed10b3eb8e52082af157410b4
Anatoly Ivashov <tonyshow@mail.ru>
2023-02-03 19:51:55 +0200

    Rebase to FSD Architecture + absolute paths config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react'
 import { Link } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTheme } from './providers/ThemeProvider'
 import './styles/index.scss'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { useTheme } from './theme/useTheme'
-import { classNames } from './helpers/classNames/classNames'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
@@ -22,8 +22,8 @@ const App = () => {
       <Link to='/about'>About</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageAsync />} />
-          <Route path='/' element={<MainPageAsync />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/' element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
