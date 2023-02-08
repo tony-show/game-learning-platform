4959e92a326b071bbe0792d9e14fcefe31d83705
Anatoly Ivashov <tonyshow@mail.ru>
2023-02-08 18:02:49 +0200

    Added babel + i18next extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,27 @@ import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const babelLoader = {
+    test: /\.(j|t)sx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            },
+          ],
+          // […] your other plugins […]
+        ],
+      },
+    },
+  }
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -41,5 +62,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     },
   }
 
-  return [typescriptLoader, styleLoader, SVGLoader, fileLoader]
+  return [babelLoader, typescriptLoader, styleLoader, SVGLoader, fileLoader]
 }
