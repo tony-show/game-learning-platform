ad0fb0f11cf1d06f422204d74757f76fc58f9b07
Anatoly Ivashov <tonyshow@mail.ru>
2023-02-08 17:40:58 +0200

    Added Hot replace for styles and components

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,3 +1,4 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
@@ -19,5 +20,7 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin(),
   ]
 }
