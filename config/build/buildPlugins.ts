d2bb8ef5be20aa20b296f5e253539d84e02a84a1
Anatoly Ivashov <tonyshow@mail.ru>
2023-02-17 11:42:45 +0200

    fix config of dev build plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -10,7 +10,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -22,10 +22,15 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ]
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(new ReactRefreshWebpackPlugin())
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }))
+  }
+
+  return plugins
 }
